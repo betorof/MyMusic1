5a80a809f822cbd121bea394693e55ecfb524cc0
dappa <94505216+ReyyNada@users.noreply.github.com>
2023-12-07 01:57:13 +0700

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -46,7 +46,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [  
-            InlineKeyboardButton(text=_["SA_B_4"], url=f"https://t.me/mahadappa",
+            InlineKeyboardButton(text=_["SA_B_4"], url=f"https://t.me/mahadappa"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -66,7 +66,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [  
-            InlineKeyboardButton(text=_["SA_B_4"], url=f"https://t.me/mahadappa",
+            InlineKeyboardButton(text=_["SA_B_4"], url=f"https://t.me/mahadappa"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
